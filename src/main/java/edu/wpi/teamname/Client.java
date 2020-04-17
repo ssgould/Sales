325fb3d4969b8a789cb1823c852c51f462c22e19
Sam <ssgould25@gmail.com>
2020-04-16 22:11:59 -0400

    finished Client, preliminary Employee

--- src/main/java/edu/wpi/teamname/Client.java
+++ src/main/java/edu/wpi/teamname/Client.java
@@ -1,4 +1,55 @@
 package edu.wpi.teamname;
 
 public class Client {
+
+    private String clientID;
+    private String clientName;
+    private Employee salesEmployee;
+
+    /**
+     * constructs a client object
+     * @param clientID unique client id
+     * @param clientName name of the client
+     */
+    public Client(String clientID, String clientName, Employee salesEmployee){
+        this.clientID = clientID;
+        this.clientName = clientName;
+        this.salesEmployee = salesEmployee;
+    }
+
+    /**
+     *
+     * @return client id
+     */
+    protected String getClientID() {
+        return clientID;
+    }
+
+    protected void setClientID(String clientID) {
+        this.clientID = clientID;
+    }
+
+    /**
+     *
+     * @return client name
+     */
+    protected String getClientName(){
+        return clientName;
+    }
+
+    protected void setClientName(String clientName) {
+        this.clientName = clientName;
+    }
+
+    protected Employee getSalesEmployee() {
+        return salesEmployee;
+    }
+
+    protected void setSalesEmployee(Employee salesEmployee) {
+        this.salesEmployee = salesEmployee;
+    }
+
+    private double totalSales(){
+        return 0;
+    }
 }
--- src/main/java/edu/wpi/teamname/Employee.java
+++ src/main/java/edu/wpi/teamname/Employee.java
@@ -1,4 +1,16 @@
 package edu.wpi.teamname;
 
+import java.util.ArrayList;
+
 public abstract class Employee {
+
+    private ArrayList<Client> clients;
+
+    public boolean addClient() {
+       //insert
+       return false;
+    }
+    private ArrayList<Client> getClients() {
+       return new ArrayList<Client>();
+    }
 }
